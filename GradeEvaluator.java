c744345777fbc3535cd52e526aa64bb34f107772
Rithvik Yaddanapudi <rithvikyaddanapudi@gmail.com>
2026-09-14 16:00:11 -0500

    Add files via upload

--- GradeEvaluator.java
+++ GradeEvaluator.java
@@ -1,5 +1,23 @@
 package beginnerProjects;
-
+import java.util.*;
 public class GradeEvaluator {
+	public static void main (String []args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Enter your grade: ");
+		double myGrade = input.nextDouble();
+		
+		if (myGrade >= 90) {
+			System.out.println("Your grade is A");
+		}	else if (myGrade >= 80) {
+			System.out.println("Your grade is B");
+		}	else if (myGrade >= 70) {
+			System.out.println("Your grade is C");
+		}	else if (myGrade >= 60) {
+			System.out.println("Your grade is D");
+		}	else {
+			System.out.println("Your grade is F");
+		}
+		
+	}
 
 }
