5b2e2b676399c0bcd059eb020a471ea5388d3c0b
Rithvik Yaddanapudi <rithvikyaddanapudi@gmail.com>
2026-09-14 16:23:31 -0500

    Add files via upload

--- GradeEvaluator.java
+++ GradeEvaluator.java
@@ -1,4 +1,5 @@
 package beginnerProjects;
+
 import java.util.*;
 public class GradeEvaluator {
 	public static void main (String []args) {
@@ -6,16 +7,36 @@ public class GradeEvaluator {
 		System.out.print("Enter your grade: ");
 		double myGrade = input.nextDouble();
 		
-		if (myGrade >= 90) {
-			System.out.println("Your grade is A");
-		}	else if (myGrade >= 80) {
-			System.out.println("Your grade is B");
-		}	else if (myGrade >= 70) {
-			System.out.println("Your grade is C");
-		}	else if (myGrade >= 60) {
-			System.out.println("Your grade is D");
+		if (myGrade >= 0 && myGrade <= 100) {
+			if (myGrade >= 97) {
+				System.out.println("Your grade is A+");
+			}	else if (myGrade >= 93) {
+				System.out.println("Your grade is A");
+			}	else if (myGrade >= 90) {
+				System.out.println("Your grade is A-");
+			}	else if (myGrade >= 87) {
+				System.out.println("Your grade is B+");
+			}	else if (myGrade >= 83) {
+				System.out.println("Your grade is B");
+			}	else if (myGrade >= 80) {
+				System.out.println("Your grade is B-");
+			}	else if (myGrade >= 77) {
+				System.out.println("Your grade is C+");
+			}	else if (myGrade >= 73) {
+				System.out.println("Your grade is C");
+			}	else if (myGrade >= 70) {
+				System.out.println("Your grade is C-");
+			}	else if (myGrade >= 67) {
+				System.out.println("Your grade is D+");
+			}	else if (myGrade >= 63) {
+				System.out.println("Your grade is D");
+			}	else if (myGrade >= 60) {
+				System.out.println("Your grade is D-");
+			}	else {
+				System.out.println("Your grade is F");
+			}
 		}	else {
-			System.out.println("Your grade is F");
+			System.out.println("Invalid number");
 		}
 		
 	}
